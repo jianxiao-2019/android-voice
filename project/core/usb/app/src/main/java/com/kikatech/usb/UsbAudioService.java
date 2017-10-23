7b248a2fee70f6508f9a54d7a49a138bf06c6da7
tianli <li.tian@kikatech.com>
2017-10-23 14:52:20 +0800

    add usb project

---
+++ project/core/usb/app/src/main/java/com/kikatech/usb/UsbAudioService.java
@@ -0,0 +1,8 @@
+package com.kikatech.usb;
+
+/**
+ * Created by tianli on 17-10-23.
+ */
+
+public class UsbAudioService {
+}
---
+++ project/core/usb/app/src/main/java/com/kikatech/usb/ui/UsbActivity.java
@@ -0,0 +1,10 @@
+package com.kikatech.usb.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by tianli on 17-10-23.
+ */
+
+public class UsbActivity extends AppCompatActivity{
+}
