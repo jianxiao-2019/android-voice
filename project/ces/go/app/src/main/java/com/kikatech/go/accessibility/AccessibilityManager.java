03d5b5aa4ebd651c2e751a0b605de2ec59b8d2f2
jason <chuen015@gmail.com>
2017-10-28 16:20:40 +0800

    Break accessibility reflection function call

--- project/ces/go/app/src/main/java/com/kikatech/go/accessibility/AccessibilityManager.java
+++ project/ces/go/app/src/main/java/com/kikatech/go/accessibility/AccessibilityManager.java
@@ -101,6 +101,7 @@ public class AccessibilityManager {
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
+                        break;
                     }
                 }
             }
