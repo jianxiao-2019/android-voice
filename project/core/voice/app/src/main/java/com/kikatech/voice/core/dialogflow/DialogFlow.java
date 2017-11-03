705f61fc4e4a1e340fe0078583818c6b31ae8933
tianli <li.tian@kikatech.com>
2017-11-03 15:12:16 +0800

    dialog flow

--- project/core/voice/app/src/main/java/com/kikatech/voice/core/dialogflow/DialogFlow.java
+++ project/core/voice/app/src/main/java/com/kikatech/voice/core/dialogflow/DialogFlow.java
@@ -1,6 +1,17 @@
 package com.kikatech.voice.core.dialogflow;
 
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+
 import com.kikatech.voice.VoiceConfiguration;
+import com.kikatech.voice.core.dialogflow.intent.Intent;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by tianli on 17-10-28.
@@ -10,19 +21,60 @@ public class DialogFlow {
 
     private Agent mAgent;
 
-    private DialogFlow(Agent agent){
+    private HashMap<String, List<DialogObserver>> mSubscribers = new HashMap<>();
+    private ExecutorService mExecutor = Executors.newSingleThreadExecutor();
+
+    private DialogFlow(Agent agent) {
         mAgent = agent;
     }
 
-    public static DialogFlow getInstance(VoiceConfiguration conf){
+    public static DialogFlow getInstance(VoiceConfiguration conf) {
         DialogFlow flow = new DialogFlow(conf.getAgent().create());
         return flow;
     }
 
-    public void talk(String words){
+    public void talk(final String words) {
+        if (!TextUtils.isEmpty(words)) {
+            mExecutor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = mAgent.query(words);
+                    if(intent != null) {
+                        onIntent(intent);
+                    }
+                }
+            });
+        }
     }
 
     public void register(String scene, DialogObserver observer) {
+        if (!TextUtils.isEmpty(scene) && observer != null) {
+            if (mSubscribers.get(scene) == null) {
+                List<DialogObserver> objectList = new ArrayList<>();
+                objectList.add(observer);
+                mSubscribers.put(scene, objectList);
+            } else {
+                if (!mSubscribers.get(scene).contains(observer)) {
+                    mSubscribers.get(scene).add(observer);
+                }
+            }
+        }
+    }
+
+    public void unregister(String scene, DialogObserver object) {
+        if (!TextUtils.isEmpty(scene) && object != null) {
+            if (mSubscribers.get(scene) != null) {
+                mSubscribers.get(scene).remove(object);
+            }
+        }
+    }
+
+    private void onIntent(Intent intent) {
+        String scene = intent.getScene();
+        List<DialogObserver> subscribers = mSubscribers.get(scene);
+        for(DialogObserver subscriber : subscribers) {
+            subscriber.onIntent(intent);
+        }
     }
 
 }
