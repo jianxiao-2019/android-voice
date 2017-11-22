518b18221d42858282e4afd30650493b28518646
Skeeter <skeeterwang0929@gmail.com>
2017-11-22 17:58:31 +0800

    enable immersive mode

--- project/ces/go/app/src/main/java/com/kikatech/go/ui/BaseActivity.java
+++ project/ces/go/app/src/main/java/com/kikatech/go/ui/BaseActivity.java
@@ -1,13 +1,16 @@
 package com.kikatech.go.ui;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
 import android.widget.Toast;
 
 /**
  * @author jasonli Created on 2017/10/24.
  */
 
-public class BaseActivity extends Activity {
+public abstract class BaseActivity extends Activity {
 
     protected void showToast(final String message) {
         __showToast(message, Toast.LENGTH_SHORT);
@@ -26,4 +29,34 @@ public class BaseActivity extends Activity {
         });
     }
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        hideSystemUI();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+    }
+
+    private void showSystemUI() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
 }
