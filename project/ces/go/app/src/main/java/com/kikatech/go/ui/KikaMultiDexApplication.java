a2a5b2e6a021e30be53584cdbfc35cbc46997614
Skeeter <skeeterwang0929@gmail.com>
2017-12-11 11:55:39 +0800

    [fabric] add fabric supporting

--- project/ces/go/app/src/main/java/com/kikatech/go/ui/KikaMultiDexApplication.java
+++ project/ces/go/app/src/main/java/com/kikatech/go/ui/KikaMultiDexApplication.java
@@ -6,6 +6,10 @@ import android.os.StrictMode;
 import android.support.multidex.MultiDex;
 import android.support.multidex.MultiDexApplication;
 
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
 /**
  * @author SkeeterWang Created on 2017/11/2.
  */
@@ -23,6 +27,7 @@ public class KikaMultiDexApplication extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         sContext = this;
 
         StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
