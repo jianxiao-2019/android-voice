8f984c891ef335a0d5f047e9f7d6151660d4d6c0
Brad Chang <cutebrady@gmail.com>
2017-12-12 13:56:39 +0800

    [log] Add file log for Voice MVP

--- voiceSDK/src/main/java/com/kikatech/voice/util/log/Logger.java
+++ voiceSDK/src/main/java/com/kikatech/voice/util/log/Logger.java
@@ -8,29 +8,54 @@ import android.util.Log;
 
 public class Logger {
     public static final boolean DEBUG = true;
+    private final static boolean ENABLE_FILE_LOG = DEBUG;
     private static final String TAG = "KikaVoiceMVP";
+
+    private final static String LOG_FOLDER = LogUtil.LOG_FOLDER;
+    public final static String LOG_FILE = "%s_voice_mvp.txt";
+    private static int mFileLoggerId = -1;
 
     public static void v(String message) {
         if (DEBUG) {
             Log.v(TAG, message);
+            writeFileLog(message);
         }
     }
 
     public static void d(String message) {
         if (DEBUG) {
             Log.d(TAG, message);
+            writeFileLog(message);
         }
     }
 
     public static void i(String message) {
-        Log.i(TAG, message);
+        if (DEBUG) {
+            Log.i(TAG, message);
+            writeFileLog(message);
+        }
     }
 
     public static void w(String message) {
-        Log.w(TAG, message);
+        if (DEBUG) {
+            Log.w(TAG, message);
+            writeFileLog(message);
+        }
     }
 
     public static void e(String message) {
-        Log.e(TAG, message);
+        if (DEBUG) {
+            Log.e(TAG, message);
+            writeFileLog(message);
+        }
+    }
+
+    private static void writeFileLog(String message) {
+        if (ENABLE_FILE_LOG) {
+            if (mFileLoggerId == -1) {
+                mFileLoggerId = FileLoggerUtil.getIns().configFileLogger(LOG_FOLDER, LOG_FILE);
+            }
+            FileLoggerUtil.getIns().writeLogToFile(mFileLoggerId, message);
+        }
     }
 }
