1265cb22f6d8d372c6cd64b7c404b5f8971e9874
Brad Chang <cutebrady@gmail.com>
2017-12-23 13:10:18 +0800

    [snowboy] Change wakeup hot word to Hi Kika

--- voiceSDK/src/main/java/ai/kitt/snowboy/Constants.java
+++ voiceSDK/src/main/java/ai/kitt/snowboy/Constants.java
@@ -5,11 +5,10 @@ import android.os.Environment;
 import java.io.File;
 
 public class Constants {
-    private static final String UMDL_ALEXA = "alexa.umdl";
-    private static final String UMDL_SNOWBOY = "snowboy.umdl";
+    private static final String UMDL_HI_KIKA = "hotword_hi-kika-gstyle.umdl";
     public static final String ASSETS_RES_DIR = "snowboy";
     public static final String DEFAULT_WORK_SPACE = Environment.getExternalStorageDirectory().getAbsolutePath() + "/snowboy/";
-    public static final String ACTIVE_UMDL = UMDL_SNOWBOY;
+    public static final String ACTIVE_UMDL = UMDL_HI_KIKA;
     public static final String ACTIVE_RES = "common.res";
     public static final String SAVE_AUDIO = Constants.DEFAULT_WORK_SPACE + File.separatorChar + "recording.pcm";
     public static final int SAMPLE_RATE = 16000;
