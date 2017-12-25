18be518ca427024d64d7437927834660e7f29518
Skeeter <skeeterwang0929@gmail.com>
2017-12-25 14:25:59 +0800

    [dialogFlow] [scene] [navigation] use sys.any instead of sys.location on DialogFlow

--- project/ces/go/app/src/main/java/com/kikatech/go/dialogflow/navigation/NaviSceneUtil.java
+++ project/ces/go/app/src/main/java/com/kikatech/go/dialogflow/navigation/NaviSceneUtil.java
@@ -10,12 +10,8 @@ import android.text.TextUtils;
 
 import com.kikatech.go.navigation.NavigationManager;
 import com.kikatech.go.navigation.provider.BaseNavigationProvider;
-import com.kikatech.go.ui.KikaAlphaUiActivity;
 import com.kikatech.go.util.LogUtil;
 import com.kikatech.go.util.StringUtil;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -25,57 +21,23 @@ import java.util.ArrayList;
 
 public class NaviSceneUtil {
 
-    private final static String PRM_ADDRESS = "address";
-    private final static String PRM_LOCATION = "location";
-    private final static String PRM_ATTRACTION_US = "place-attraction-us";
-    private final static String PRM_SPECIFIC_LOC = "specific-loc";
-    private final static String PRM_CITY = "city";
-    private final static String PRM_SUBADMIN_AREA = "subadmin-area";
-    private final static String PRM_ADMIN_AREA = "admin-area";
-    private final static String PRM_ZIP_CODE = "zip-code";
-
-    private final static String KEY_STREET_ADDR = "street-address";
-    private final static String KEY_BUSINESS_NAME = "business-name";
-    private final static String KEY_SHORTCUT = "shortcut";
-
-    private final static String[] PRM_ARRAY = {PRM_CITY, PRM_LOCATION, PRM_ADDRESS, PRM_ATTRACTION_US, PRM_SPECIFIC_LOC, PRM_SUBADMIN_AREA, PRM_ZIP_CODE, KEY_BUSINESS_NAME};
-    private final static String[] LOC_KEYS = {PRM_CITY, KEY_STREET_ADDR, KEY_BUSINESS_NAME, KEY_SHORTCUT, PRM_SUBADMIN_AREA, PRM_ADMIN_AREA, PRM_ZIP_CODE};
+    private final static String PARAM_DESTINATION = "destination";
 
     private static boolean sNavigating = false;
 
-    public static String parseAddress(@NonNull Bundle parm) {
-        if (LogUtil.DEBUG) LogUtil.log("NaviSceneUtil", "parm : " + parm);
-        StringBuilder fullAddress = new StringBuilder();
-        for (String key : PRM_ARRAY) {
-            String addr = parm.getString(key);
-            if (!TextUtils.isEmpty(addr)) {
-                String location = "";
-                boolean parseSuccess = false;
-                boolean validJson = true;
-                try {
-                    JSONObject json = new JSONObject(addr);
-                    for (String locKey : LOC_KEYS) {
-                        if (json.has(locKey)) {
-                            location = json.getString(locKey);
-                            if (!TextUtils.isEmpty(location)) {
-                                fullAddress.append(location).append(" ");
-                                parseSuccess = true;
-                            }
-                        }
-                    }
-                } catch (JSONException ignored) {
-                    validJson = false;
-                }
-                if (!parseSuccess && !validJson) {
-                    fullAddress.append(addr.replace("\"", ""));
-                }
-                if (LogUtil.DEBUG)
-                    LogUtil.log("NaviSceneUtil", "parseAddress : " + fullAddress.toString());
+    public static String parseAddress(@NonNull Bundle params) {
+        if (LogUtil.DEBUG) {
+            LogUtil.log("NaviSceneUtil", "params : " + params);
+        }
+        String destination = params.getString(PARAM_DESTINATION);
+        if (!TextUtils.isEmpty(destination)) {
+            try {
+                destination = destination.substring(1, destination.length() - 1);
+                destination = StringUtil.upperCaseFirstWord(destination);
+            } catch (Exception ignore) {
             }
         }
-        if (LogUtil.DEBUG)
-            LogUtil.log("NaviSceneUtil", "parseAddress : " + fullAddress.toString() + ", parm:" + parm.keySet().size());
-        return StringUtil.upperCaseFirstWord(fullAddress.toString());
+        return destination;
     }
 
     public synchronized static void navigateToLocation(Context ctx, String loc) {
@@ -95,7 +57,9 @@ public class NaviSceneUtil {
             return;
         }
 
-        if (LogUtil.DEBUG) LogUtil.log("NaviSceneUtil", "Start to stop navigation ..., targetClz:" + targetClz);
+        if (LogUtil.DEBUG) {
+            LogUtil.log("NaviSceneUtil", "Start to stop navigation ..., targetClz:" + targetClz);
+        }
 
         final Handler uiHandler = new Handler(Looper.getMainLooper());
 
