f7005e263e1f572818e13c5d46ffaa8cf8d7493f
Brad Chang <cutebrady@gmail.com>
2018-01-02 14:37:00 +0800

    [intent] Support stop actions for local intent

--- project/ces/go/app/src/main/java/com/kikatech/go/dialogflow/apiai/LocalProcessorAgent.java
+++ project/ces/go/app/src/main/java/com/kikatech/go/dialogflow/apiai/LocalProcessorAgent.java
@@ -1,8 +1,12 @@
 package com.kikatech.go.dialogflow.apiai;
 
+import com.kikatech.go.dialogflow.stop.SceneActions;
+import com.kikatech.go.dialogflow.stop.SceneStopIntent;
+import com.kikatech.go.util.LogUtil;
 import com.kikatech.voice.core.dialogflow.Agent;
 import com.kikatech.voice.core.dialogflow.intent.Intent;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -11,9 +15,36 @@ import java.util.Map;
  */
 
 public class LocalProcessorAgent extends Agent {
+    private static final String TAG = "LocalProcessorAgent";
+
     @Override
     public Intent query(String words, String[] nBestWords, Map<String, List<String>> entities, byte queryType) {
-        return new Intent(Intent.AS_PREV_SCENE, Intent.ACTION_USER_INPUT, words, nBestWords);
+        Intent intent = getCancelIntent(nBestWords);
+        if (intent == null) {
+            intent = new Intent(Intent.AS_PREV_SCENE, Intent.ACTION_USER_INPUT, words, nBestWords);
+        }
+
+        if (LogUtil.DEBUG) LogUtil.log(TAG, "Local Intent : " + intent);
+        return intent;
+    }
+
+    private Intent getCancelIntent(String[] nBestWords) {
+        if (nBestWords == null || nBestWords.length == 0) {
+            return null;
+        }
+
+        List<String> words = new ArrayList<>(nBestWords.length);
+        for (String s : nBestWords) {
+            words.add(s.toLowerCase().trim());
+        }
+
+        for (String stop : LocalProcessorCommands.CANCEL) {
+            if (words.contains(stop)) {
+                if (LogUtil.DEBUG) LogUtil.log(TAG, "Find stop word : " + stop);
+                return new Intent(SceneStopIntent.SCENE, SceneActions.STOP_ACTION, stop, nBestWords);
+            }
+        }
+        return null;
     }
 
     @Override
---
+++ project/ces/go/app/src/main/java/com/kikatech/go/dialogflow/apiai/LocalProcessorCommands.java
@@ -0,0 +1,17 @@
+package com.kikatech.go.dialogflow.apiai;
+
+/**
+ * Created by brad_chang on 2018/1/2.
+ */
+
+class LocalProcessorCommands {
+    static final String[] CANCEL = new String[]{
+            "cancel",
+            "stop",
+            "abort",
+            "stop it",
+            "cancel it",
+            "go to main page",
+            "back to main page",
+    };
+}
