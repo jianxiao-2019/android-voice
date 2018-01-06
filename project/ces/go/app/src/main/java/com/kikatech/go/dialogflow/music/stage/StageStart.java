9cedaccf275e0d5dae9d9e013af895683d204397
Skeeter <skeeterwang0929@gmail.com>
2018-01-06 14:04:47 +0800

    [dialogFlow] [scene-music] add play music tts

--- project/ces/go/app/src/main/java/com/kikatech/go/dialogflow/music/stage/StageStart.java
+++ project/ces/go/app/src/main/java/com/kikatech/go/dialogflow/music/stage/StageStart.java
@@ -3,6 +3,8 @@ package com.kikatech.go.dialogflow.music.stage;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 
+import com.kikatech.go.dialogflow.SceneUtil;
+import com.kikatech.go.dialogflow.model.TtsText;
 import com.kikatech.go.music.MusicManager;
 import com.kikatech.voice.core.dialogflow.scene.ISceneFeedback;
 import com.kikatech.voice.core.dialogflow.scene.SceneBase;
@@ -24,11 +26,23 @@ public class StageStart extends BaseMusicStage {
     }
 
     @Override
-    public void prepare() {
+    public void doAction() {
+        onStageActionStart();
+        action();
     }
 
     @Override
     public void action() {
+        String tmp = "OK, playing music.";
+        String[] uiAndTtsText = new String[] {tmp, tmp};
+        if (uiAndTtsText.length > 0) {
+            String uiText = uiAndTtsText[0];
+            String ttsText = uiAndTtsText[1];
+            TtsText tText = new TtsText(uiText);
+            Bundle args = new Bundle();
+            args.putParcelable(SceneUtil.EXTRA_TTS_TEXT, tText);
+            speak(ttsText, args);
+        }
         MusicManager.getIns().play();
     }
 
