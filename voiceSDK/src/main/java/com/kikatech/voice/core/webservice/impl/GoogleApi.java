acf1492d7da5e1d62d0e0011fafe99f0e8aacb63
ryanlin <ryan.lin@kikatech.com>
2018-05-25 11:39:39 +0800

    First buildable version.

--- voiceSDK/src/main/java/com/kikatech/voice/core/webservice/impl/GoogleApi.java
+++ voiceSDK/src/main/java/com/kikatech/voice/core/webservice/impl/GoogleApi.java
@@ -6,7 +6,39 @@ import android.util.Log;
 import com.kikatech.voice.core.webservice.IWebSocket;
 import com.kikatech.voice.service.conf.VoiceConfiguration;
 
+import com.google.auth.Credentials;
+import com.google.auth.oauth2.AccessToken;
+import com.google.auth.oauth2.GoogleCredentials;
+
+import com.google.auth.Credentials;
+import com.google.auth.oauth2.AccessToken;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.speech.v1.RecognitionAudio;
+import com.google.cloud.speech.v1.RecognitionConfig;
+import com.google.cloud.speech.v1.RecognizeRequest;
+import com.google.cloud.speech.v1.RecognizeResponse;
 import com.google.cloud.speech.v1.SpeechGrpc;
+import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
+import com.google.cloud.speech.v1.SpeechRecognitionResult;
+import com.google.cloud.speech.v1.StreamingRecognitionConfig;
+import com.google.cloud.speech.v1.StreamingRecognitionResult;
+import com.google.cloud.speech.v1.StreamingRecognizeRequest;
+import com.google.cloud.speech.v1.StreamingRecognizeResponse;
+import com.google.protobuf.ByteString;
+
+import io.grpc.CallOptions;
+import io.grpc.Channel;
+import io.grpc.ClientCall;
+import io.grpc.ClientInterceptor;
+import io.grpc.ClientInterceptors;
+import io.grpc.ManagedChannel;
+import io.grpc.Metadata;
+import io.grpc.MethodDescriptor;
+import io.grpc.Status;
+import io.grpc.StatusException;
+import io.grpc.internal.DnsNameResolverProvider;
+import io.grpc.okhttp.OkHttpChannelProvider;
+import io.grpc.stub.StreamObserver;
 
 import java.util.Collections;
 import java.util.List;
@@ -35,7 +67,7 @@ public class GoogleApi implements IWebSocket {
 
 //    private final SpeechBinder mBinder = new SpeechBinder();
 //    private final ArrayList<Listener> mListeners = new ArrayList<>();
-    private volatile AccessTokenTask mAccessTokenTask;
+//    private volatile AccessTokenTask mAccessTokenTask;
     private SpeechGrpc.SpeechStub mApi;
     private static Handler mHandler;
 
@@ -54,9 +86,9 @@ public class GoogleApi implements IWebSocket {
                 }
             }
             if (text != null) {
-                for (Listener listener : mListeners) {
-                    listener.onSpeechRecognized(text, isFinal);
-                }
+//                for (Listener listener : mListeners) {
+//                    listener.onSpeechRecognized(text, isFinal);
+//                }
             }
         }
 
@@ -71,14 +103,15 @@ public class GoogleApi implements IWebSocket {
         }
 
     };
+
     @Override
-    public void connect(VoiceConfiguration.ConnectionConfiguration conf) {
+    public void connect(VoiceConfiguration voiceConfiguration) {
 
     }
 
     @Override
     public void release() {
-
+        CallOptions callOptions = null;
     }
 
     @Override
