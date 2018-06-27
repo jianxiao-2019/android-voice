093ba8d9854df01076999bdecb82391b2c008724
Skeeter <skeeterwang0929@gmail.com>
2018-06-27 16:12:26 +0800

    update check screen lock impl

--- project/ces/go/app/src/main/java/com/kikatech/go/util/KeyguardUtil.java
+++ project/ces/go/app/src/main/java/com/kikatech/go/util/KeyguardUtil.java
@@ -2,6 +2,7 @@ package com.kikatech.go.util;
 
 import android.app.KeyguardManager;
 import android.content.Context;
+import android.os.PowerManager;
 
 /**
  * @author SkeeterWang Created on 2018/6/5.
@@ -9,7 +10,18 @@ import android.content.Context;
 
 public class KeyguardUtil {
     public static boolean isScreenLocked(Context context) {
+        boolean inKeyguardRestrictedInputMode = __inKeyguardRestrictedInputMode(context);
+        boolean isPowerManagerLocked = __isPowerManagerLocked(context);
+        return inKeyguardRestrictedInputMode || isPowerManagerLocked;
+    }
+
+    private static boolean __inKeyguardRestrictedInputMode(Context context) {
         KeyguardManager keyguardMgr = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
         return keyguardMgr != null && keyguardMgr.inKeyguardRestrictedInputMode();
+    }
+
+    private static boolean __isPowerManagerLocked(Context context) {
+        PowerManager powerMgr = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        return powerMgr != null && !powerMgr.isInteractive();
     }
 }
