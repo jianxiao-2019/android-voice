6067f9ab74a8a713e9b80aab92fc3333fc7ef805
ianfan0704 <ian.fan@kikatech.com>
2018-09-10 09:24:09 +0800

    [NC] update NC 50736 version

--- project/Tester/VoiceSdkTester/app/src/main/java/com/kikatech/voicesdktester/activities/MyApplication.java
+++ project/Tester/VoiceSdkTester/app/src/main/java/com/kikatech/voicesdktester/activities/MyApplication.java
@@ -5,9 +5,6 @@ import android.content.ComponentName;
 import android.content.pm.PackageManager;
 
 import com.kikatech.voice.util.log.Logger;
-import com.kikatech.voicesdktester.utils.PreferenceUtil;
-
-import static com.kikatech.voicesdktester.utils.PreferenceUtil.KEY_ENABLE_DEBUG_APP;
 
 /**
  * Created by ryanlin on 01/02/2018.
@@ -19,14 +16,14 @@ public class MyApplication extends Application {
         super.onCreate();
 //        LeakCanary.install(this);
 
-        if (PreferenceUtil.getBoolean(this, KEY_ENABLE_DEBUG_APP, false)) {
+//        if (PreferenceUtil.getBoolean(this, KEY_ENABLE_DEBUG_APP, false)) {
             Logger.d("MyApplication enable");
             PackageManager p = getPackageManager();
             p.setComponentEnabledSetting(new ComponentName(this, MainActivity.class), PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
-        } else {
-            Logger.d("MyApplication disable");
-            PackageManager p = getPackageManager();
-            p.setComponentEnabledSetting(new ComponentName(this, MainActivity.class), PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
-        }
+//        } else {
+//            Logger.d("MyApplication disable");
+//            PackageManager p = getPackageManager();
+//            p.setComponentEnabledSetting(new ComponentName(this, MainActivity.class), PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+//        }
     }
 }
