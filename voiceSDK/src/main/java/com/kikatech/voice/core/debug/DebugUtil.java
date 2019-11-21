a1ced3299ea1369a4de6546497ec99ddc7f523a0
YANG Qiu <qiu.yang@micro-lightning.com>
2019-11-21 20:30:57 +0800

    add connect status

--- voiceSDK/src/main/java/com/kikatech/voice/core/debug/DebugUtil.java
+++ voiceSDK/src/main/java/com/kikatech/voice/core/debug/DebugUtil.java
@@ -29,12 +29,19 @@ public class DebugUtil {
 
     private static final String PRE_PHONE = "Phone_";
     private static final String PRE_KIKA_GO = "Kikago_";
-    private static final String PRE_LOCAL = "Local_";
+    private static final String PRE_LOCAL_BTB0 = "Local_btb0_";
     private static final String PRE_UNKNOWN = "Unknown_";
+    private static final String PRE_LOCAL_PM0 = "Local_pm0_";
+    private static final String PRE_LOCAL_PM180 = "Local_pm180_";
+    private static final String PRE_LOCAL_BTB180 = "Local_btb180_";
+    private static final String PRE_LOCAL_MS = "Local_ms_";
+
 
     private static boolean sIsDebug = false;
 
     private static String sAsrAudioFilePath;
+
+    private static String status;
 
     private static LongSparseArray<String> cidToFilePath = new LongSparseArray<>();
 
@@ -45,6 +52,10 @@ public class DebugUtil {
             return;
         }
         cidToFilePath.clear();
+    }
+
+    public static void getStatus(String str) {
+        status = str;
     }
 
     public static boolean isDebug() {
@@ -66,7 +77,18 @@ public class DebugUtil {
         } else if (source.getClass().getSimpleName().contains("Kika")) {
             return PRE_KIKA_GO;
         } else if (source.getClass().getSimpleName().contains("Local")) {
-            return PRE_LOCAL;
+            if (status.contains("VL630-PM-0")){
+                return PRE_LOCAL_PM0;
+            } else if (status.contains("VL630-PM-180")){
+                return PRE_LOCAL_PM180;
+            } else if (status.contains("VL630-BTB-0")) {
+                return PRE_LOCAL_BTB0;
+            } else if (status.contains("VL630-BTB-180")) {
+                return PRE_LOCAL_BTB180;
+            } else if (status.contains("VL630-MS-MS")) {
+                return PRE_LOCAL_MS;
+            }
+            return PRE_UNKNOWN;
         }
         return PRE_UNKNOWN;
     }
